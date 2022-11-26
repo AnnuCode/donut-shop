7bd1e6a5e52940657ea497f14b9eaea1c295ac4a
Annu Singh <annu4444.as@gmail.com>
2022-11-26 21:11:44 +0530

    donut list component

---
+++ src/app/admin/containers/donut-list/donut-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donut-list',
+  templateUrl: './donut-list.component.html',
+  styleUrls: ['./donut-list.component.css']
+})
+export class DonutListComponent {
+
+}
