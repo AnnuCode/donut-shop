43ecc87c6d02378c33a7a00cf74db3278bacd555
Annu Singh <annu4444.as@gmail.com>
2022-11-27 20:59:32 +0530

    progress

--- src/app/admin/containers/donut-list/donut-list.component.ts
+++ src/app/admin/containers/donut-list/donut-list.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Donut } from '../../model/donut.model';
 
 @Component({
   selector: 'app-donut-list',
   templateUrl: './donut-list.component.html',
   styleUrls: ['./donut-list.component.css']
 })
-export class DonutListComponent {
+export class DonutListComponent implements OnInit{
+  donuts!: Donut[]
+  donut!: Donut
 
+  constructor(){}
+  ngOnInit(): void {
+    this.donuts = [
+      {
+        id: 'asdfsd',
+        name: 'Chocolate',
+        description: 'chocolate wonderland'
+      },
+      {
+        id: 'xcvsdf',
+        name: 'Vanilla',
+        description: 'Tasty vanilla'
+      },
+      {
+        id: 'sdfgvd',
+        name: 'Chocolate Caramel',
+        description: 'Chocolate dazzled with caramel'
+      }
+    ]
+
+    this.donut = this.donuts[0]
+  }
 }
---
+++ src/app/admin/model/donut.model.ts
@@ -0,0 +1,5 @@
+export interface Donut {
+    id: string,
+    name: string,
+    description: string
+}
