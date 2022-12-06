c35abd7134fb7f8295a9a51276f76d8454610733
Annu Singh <annu4444.as@gmail.com>
2022-12-06 11:28:12 +0530

    setting up proxy

--- src/app/admin/services/donut.service.ts
+++ src/app/admin/services/donut.service.ts
@@ -5,45 +5,7 @@ import { Donut } from '../model/donut.model';
   providedIn: 'root',
 })
 export class DonutService {
-  private donuts: Donut[] = [
-    {
-      id: 'asdfsd',
-      name: 'Chocolate',
-      description: 'chocolate wonderland',
-      icon: 'just-chocolate',
-      price: 119,
-      promo: 'limited',
-    },
-    {
-      id: 'xcvsdf',
-      name: 'Vanilla',
-      description: 'Tasty vanilla',
-      icon: 'vanilla-sundae',
-      price: 129,
-      promo: 'new',
-    },
-    {
-      id: 'sdfgvd',
-      name: 'Chocolate Caramel',
-      description: 'Chocolate dazzled with caramel',
-      icon: 'caramel-swirl',
-      price: 129,
-    },
-    {
-      id: 'fsjner',
-      name: 'Sour Supreme',
-      description: 'For the sour lovers',
-      icon: 'sour-supreme',
-      price: 139,
-    },
-    {
-      id: 'lkjief',
-      name: 'Zesty Lemon',
-      description: 'For the lemon lovers',
-      icon: 'zesty-lemon',
-      price: 129,
-    },
-  ];
+  private donuts: Donut[] = [];
   constructor() {}
 
   read() {
